909966e97ffda4880438140a58a2c563b93abd94
Krzysiek <k.wiench@protonmail.com>
2024-03-21 13:49:43 +0100

    fixed calcCost function

--- src/js/carTrip.js
+++ src/js/carTrip.js
@@ -5,10 +5,17 @@ class Car extends Trip {
   typeOfTrip = "car";
 
   calcCost() {
+    if (
+      this._data.distance === undefined ||
+      this._data.avgFuel === undefined ||
+      this._data.fuelCost === undefined
+    ) {
+      return "-";
+    }
     return (
       Math.round(
-        ((+this.distance * +this.avgFuelConsumption) / 100) *
-          this.gasPrice *
+        ((+this._data.distance * +this._data.avgFuel) / 100) *
+          this._data.fuelCost *
           100
       ) / 100
     );
@@ -16,7 +23,9 @@ class Car extends Trip {
 
   _generateMarkup() {
     return `
-          <li id=${this._id} class="bg-gray-800/80 rounded-md p-2 border-l-4 border-pink-500 mb-2">
+          <li id=${
+            this._id
+          } class="bg-gray-800/80 rounded-md p-2 border-l-4 border-pink-500 mb-2">
               <div class="flex flex-row">
                   <div class="w-11/12">
                       <div class="flex flex-row">
@@ -26,24 +35,33 @@ class Car extends Trip {
                       <div class="flex flex-row justify-evenly">
                           <div class="trip__details pr-8">
                           <span class="trip__icon">🚗</span>
-                          <span class="trip__value">${this._data.distance}</span>
+                          <span class="trip__value">${
+                            this._data.distance
+                          }</span>
                           <span class="trip__unit">km</span>
                       </div>
                       <div class="workout__details">
                           <span class="workout__icon">⏱</span>
-                          <span class="workout__value">${this._data.duration}</span>
+                          <span class="workout__value">${
+                            this._data.duration
+                          }</span>
                           <span class="workout__unit">h</span>
                       </div>
                       <div class="trip__details">
                           <span class="trip__icon">💰</span>
-                          <span class="trip__value">${this._data.tripCost}</span>
+                          <span class="trip__value">${this.calcCost()}</span>
                           <span class="trip__unit">$</span>
                       </div>
                     </div>
                   </div>
-                  <div class="w-1/12 items-center flex">
-                      <img src=${photos} alt="photos" class="photos_icon"/>
-                  </div>
+                  ${
+                    this._uploadWithImages
+                      ? `  <div class="w-1/12 items-center flex">
+                          <img src=${photos} alt="photos" class="photos_icon" />
+                      </div>
+                   `
+                      : ""
+                  }
               </div>
           </li>
     `;
